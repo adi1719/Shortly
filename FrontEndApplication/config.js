d654ff3cd54aadf9ecf32496e193f6f450cfc327
Aditya <adiraj1719@gmail.com>
2025-03-30 15:11:40 +0530

    Remove environments for testing

--- FrontEndApplication/config.js
+++ FrontEndApplication/config.js
@@ -1,14 +1,6 @@
+// Frontend configuration
 const config = {
-  development: {
-    apiUrl: "https://shortly-tkhg.onrender.com",
-  },
-  production: {
-    apiUrl: "https://shortly-tkhg.onrender.com",
-  },
+  apiUrl: "https://shortly-tkhg.onrender.com",
 };
 
-// Get the current environment, default to development
-const env = process.env.NODE_ENV || "development";
-
-// Export the configuration for the current environment
-export default config[env];
+export default config;
